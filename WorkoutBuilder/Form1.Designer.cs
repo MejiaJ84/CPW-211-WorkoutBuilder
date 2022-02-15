91df7107f85d40e7a0141426633c23e5cf36b5cd
MejiaJ84 <jtmejia1984@gmail.com>
2022-02-14 20:07:09 -0800

    Added label, text box, and button for adding muscle group to database

--- WorkoutBuilder/Form1.Designer.cs
+++ WorkoutBuilder/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtMuscleGroup = new System.Windows.Forms.TextBox();
+            this.btnMuscleGroup = new System.Windows.Forms.Button();
+            this.lblMuscleGroup = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMuscleGroup
+            // 
+            this.txtMuscleGroup.Location = new System.Drawing.Point(176, 78);
+            this.txtMuscleGroup.Name = "txtMuscleGroup";
+            this.txtMuscleGroup.Size = new System.Drawing.Size(100, 23);
+            this.txtMuscleGroup.TabIndex = 0;
+            // 
+            // btnMuscleGroup
+            // 
+            this.btnMuscleGroup.Location = new System.Drawing.Point(315, 78);
+            this.btnMuscleGroup.Name = "btnMuscleGroup";
+            this.btnMuscleGroup.Size = new System.Drawing.Size(106, 23);
+            this.btnMuscleGroup.TabIndex = 1;
+            this.btnMuscleGroup.Text = "Add to database";
+            this.btnMuscleGroup.UseVisualStyleBackColor = true;
+            // 
+            // lblMuscleGroup
+            // 
+            this.lblMuscleGroup.AutoSize = true;
+            this.lblMuscleGroup.Location = new System.Drawing.Point(35, 82);
+            this.lblMuscleGroup.Name = "lblMuscleGroup";
+            this.lblMuscleGroup.Size = new System.Drawing.Size(106, 15);
+            this.lblMuscleGroup.TabIndex = 2;
+            this.lblMuscleGroup.Text = "Add Muscle Group";
+            this.lblMuscleGroup.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.lblMuscleGroup);
+            this.Controls.Add(this.btnMuscleGroup);
+            this.Controls.Add(this.txtMuscleGroup);
+            this.Name = "Form1";
+            this.Text = "Add to database";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox txtMuscleGroup;
+        private Button btnMuscleGroup;
+        private Label lblMuscleGroup;
     }
 }
