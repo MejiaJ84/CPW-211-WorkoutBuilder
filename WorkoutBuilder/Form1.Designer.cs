90306883b551ef407562437b3a8ac864b96d9ce7
MejiaJ84 <jtmejia1984@gmail.com>
2022-02-19 17:25:37 -0800

    Added a bunch of temporary test code to test keeping most app functionality on one form

--- WorkoutBuilder/Form1.Designer.cs
+++ WorkoutBuilder/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WorkoutBuilder
 {
-    partial class Add
+    partial class Main
     {
         /// <summary>
         ///  Required designer variable.
@@ -31,9 +31,7 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.tsmMuscleGroup = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmAddmuscleGroup = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.tsmUpdateMG = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.tsmDeleteMG = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmExercise = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmAddExercise = new System.Windows.Forms.ToolStripMenuItem();
@@ -42,7 +40,13 @@
             this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
             this.tsmDeleteExercise = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmWorkoutBuilder = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtWelcome = new System.Windows.Forms.TextBox();
+            this.gbtest1 = new System.Windows.Forms.GroupBox();
+            this.txttest1 = new System.Windows.Forms.TextBox();
+            this.btntest1 = new System.Windows.Forms.Button();
+            this.lbltest1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
+            this.gbtest1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -56,14 +60,13 @@
             this.menuStrip1.Size = new System.Drawing.Size(800, 24);
             this.menuStrip1.TabIndex = 10;
             this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
             // 
             // tsmMuscleGroup
             // 
             this.tsmMuscleGroup.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsmAddmuscleGroup,
-            this.toolStripSeparator1,
             this.tsmUpdateMG,
-            this.toolStripSeparator2,
             this.tsmDeleteMG});
             this.tsmMuscleGroup.Name = "tsmMuscleGroup";
             this.tsmMuscleGroup.Size = new System.Drawing.Size(93, 20);
@@ -72,29 +75,20 @@
             // tsmAddmuscleGroup
             // 
             this.tsmAddmuscleGroup.Name = "tsmAddmuscleGroup";
-            this.tsmAddmuscleGroup.Size = new System.Drawing.Size(180, 22);
+            this.tsmAddmuscleGroup.Size = new System.Drawing.Size(181, 22);
             this.tsmAddmuscleGroup.Text = "Add ";
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            this.tsmAddmuscleGroup.Click += new System.EventHandler(this.tsmAddmuscleGroup_Click);
             // 
             // tsmUpdateMG
             // 
             this.tsmUpdateMG.Name = "tsmUpdateMG";
-            this.tsmUpdateMG.Size = new System.Drawing.Size(180, 22);
+            this.tsmUpdateMG.Size = new System.Drawing.Size(181, 22);
             this.tsmUpdateMG.Text = "Update";
-            // 
-            // toolStripSeparator2
-            // 
-            this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
             // 
             // tsmDeleteMG
             // 
             this.tsmDeleteMG.Name = "tsmDeleteMG";
-            this.tsmDeleteMG.Size = new System.Drawing.Size(180, 22);
+            this.tsmDeleteMG.Size = new System.Drawing.Size(181, 22);
             this.tsmDeleteMG.Text = "Delete";
             // 
             // tsmExercise
@@ -112,29 +106,29 @@
             // tsmAddExercise
             // 
             this.tsmAddExercise.Name = "tsmAddExercise";
-            this.tsmAddExercise.Size = new System.Drawing.Size(180, 22);
+            this.tsmAddExercise.Size = new System.Drawing.Size(112, 22);
             this.tsmAddExercise.Text = "Add";
             // 
             // toolStripSeparator3
             // 
             this.toolStripSeparator3.Name = "toolStripSeparator3";
-            this.toolStripSeparator3.Size = new System.Drawing.Size(177, 6);
+            this.toolStripSeparator3.Size = new System.Drawing.Size(109, 6);
             // 
             // UpdateExercise
             // 
             this.UpdateExercise.Name = "UpdateExercise";
-            this.UpdateExercise.Size = new System.Drawing.Size(180, 22);
+            this.UpdateExercise.Size = new System.Drawing.Size(112, 22);
             this.UpdateExercise.Text = "Update";
             // 
             // toolStripSeparator4
             // 
             this.toolStripSeparator4.Name = "toolStripSeparator4";
-            this.toolStripSeparator4.Size = new System.Drawing.Size(177, 6);
+            this.toolStripSeparator4.Size = new System.Drawing.Size(109, 6);
             // 
             // tsmDeleteExercise
             // 
             this.tsmDeleteExercise.Name = "tsmDeleteExercise";
-            this.tsmDeleteExercise.Size = new System.Drawing.Size(180, 22);
+            this.tsmDeleteExercise.Size = new System.Drawing.Size(112, 22);
             this.tsmDeleteExercise.Text = "Delete";
             // 
             // tsmWorkoutBuilder
@@ -143,17 +137,74 @@
             this.tsmWorkoutBuilder.Size = new System.Drawing.Size(137, 20);
             this.tsmWorkoutBuilder.Text = "Open Workout Builder";
             // 
-            // Add
+            // txtWelcome
             // 
+            this.txtWelcome.Enabled = false;
+            this.txtWelcome.Location = new System.Drawing.Point(176, 51);
+            this.txtWelcome.Multiline = true;
+            this.txtWelcome.Name = "txtWelcome";
+            this.txtWelcome.Size = new System.Drawing.Size(344, 43);
+            this.txtWelcome.TabIndex = 11;
+            this.txtWelcome.Text = "Welcome to Workout Builder app version 1.00.alpha1\r\nPlease make you selection fro" +
+    "m the menu above to get started.\r\n";
+            this.txtWelcome.WordWrap = false;
+            // 
+            // gbtest1
+            // 
+            this.gbtest1.Controls.Add(this.txttest1);
+            this.gbtest1.Controls.Add(this.btntest1);
+            this.gbtest1.Controls.Add(this.lbltest1);
+            this.gbtest1.Location = new System.Drawing.Point(42, 151);
+            this.gbtest1.Name = "gbtest1";
+            this.gbtest1.Size = new System.Drawing.Size(200, 100);
+            this.gbtest1.TabIndex = 12;
+            this.gbtest1.TabStop = false;
+            this.gbtest1.Text = "Add";
+            this.gbtest1.Visible = false;
+            // 
+            // txttest1
+            // 
+            this.txttest1.Location = new System.Drawing.Point(21, 66);
+            this.txttest1.Name = "txttest1";
+            this.txttest1.Size = new System.Drawing.Size(100, 23);
+            this.txttest1.TabIndex = 2;
+            this.txttest1.Text = "test1";
+            // 
+            // btntest1
+            // 
+            this.btntest1.Location = new System.Drawing.Point(102, 22);
+            this.btntest1.Name = "btntest1";
+            this.btntest1.Size = new System.Drawing.Size(75, 23);
+            this.btntest1.TabIndex = 1;
+            this.btntest1.Text = "test1";
+            this.btntest1.UseVisualStyleBackColor = true;
+            this.btntest1.Click += new System.EventHandler(this.btntest1_Click);
+            // 
+            // lbltest1
+            // 
+            this.lbltest1.AutoSize = true;
+            this.lbltest1.Location = new System.Drawing.Point(21, 19);
+            this.lbltest1.Name = "lbltest1";
+            this.lbltest1.Size = new System.Drawing.Size(29, 15);
+            this.lbltest1.TabIndex = 0;
+            this.lbltest1.Text = "Add";
+            // 
+            // Main
+            // 
+            this.AcceptButton = this.btntest1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbtest1);
+            this.Controls.Add(this.txtWelcome);
             this.Controls.Add(this.menuStrip1);
-            this.Name = "Add";
+            this.Name = "Main";
             this.Text = "Add to database";
             this.Load += new System.EventHandler(this.Add_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.gbtest1.ResumeLayout(false);
+            this.gbtest1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -164,9 +215,7 @@
         private MenuStrip menuStrip1;
         private ToolStripMenuItem tsmMuscleGroup;
         private ToolStripMenuItem tsmAddmuscleGroup;
-        private ToolStripSeparator toolStripSeparator1;
         private ToolStripMenuItem tsmUpdateMG;
-        private ToolStripSeparator toolStripSeparator2;
         private ToolStripMenuItem tsmDeleteMG;
         private ToolStripMenuItem tsmExercise;
         private ToolStripMenuItem tsmAddExercise;
@@ -175,5 +224,10 @@
         private ToolStripSeparator toolStripSeparator4;
         private ToolStripMenuItem tsmDeleteExercise;
         private ToolStripMenuItem tsmWorkoutBuilder;
+        private TextBox txtWelcome;
+        private GroupBox gbtest1;
+        private TextBox txttest1;
+        private Button btntest1;
+        private Label lbltest1;
     }
 }
--- WorkoutBuilder/Form1.cs
+++ WorkoutBuilder/Form1.cs
@@ -1,15 +1,37 @@
 namespace WorkoutBuilder
 {
-    public partial class Add : Form
+    public partial class Main : Form
     {
-        public Add()
+        public Main()
         {
             InitializeComponent();
         }
 
         private void Add_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        private void btntest1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ive been clicked");
+        }
+
+        private void tsmAddmuscleGroup_Click(object sender, EventArgs e)
+        {
+            gbtest1.Visible = true;
+            makeInvisible(gbtest1);
+            
+        }
+        
+        private static void makeInvisible(GroupBox gbVanish)
+        {
+            gbVanish.Visible = false;
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            //menuStrip1.Items
         }
     }
 }
