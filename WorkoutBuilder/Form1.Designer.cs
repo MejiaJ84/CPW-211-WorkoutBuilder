90f6c0910c4186ece25bc2267343edd78970265f
MejiaJ84 <jtmejia1984@gmail.com>
2022-03-01 16:26:12 -0800

    Made the combo box fill with data and display when update muscle group is clicked.

--- WorkoutBuilder/Form1.Designer.cs
+++ WorkoutBuilder/Form1.Designer.cs
@@ -84,6 +84,7 @@
             this.tsmUpdateMG.Name = "tsmUpdateMG";
             this.tsmUpdateMG.Size = new System.Drawing.Size(180, 22);
             this.tsmUpdateMG.Text = "Update";
+            this.tsmUpdateMG.Click += new System.EventHandler(this.tsmUpdateMG_Click);
             // 
             // tsmDeleteMG
             // 
--- WorkoutBuilder/Form1.cs
+++ WorkoutBuilder/Form1.cs
@@ -8,7 +8,6 @@ namespace WorkoutBuilder
         public Main()
         {
             InitializeComponent();
-            
         }
 
         private void Add_Load(object sender, EventArgs e)
@@ -20,7 +19,7 @@ namespace WorkoutBuilder
         {
             if (gbAddUpdateDelete.Tag.Equals("AddMG"))
             {
-                addMuscleGroup(txtAddMuscleOrExercise);
+                AddMuscleGroup(txtAddMuscleOrExercise.Text);
                 txtAddMuscleOrExercise.Clear();
             }
         }
@@ -35,30 +34,76 @@ namespace WorkoutBuilder
         /// <param name="e"></param>
         private void tsmAddmuscleGroup_Click(object sender, EventArgs e)
         {
-            gbAddUpdateDelete.Text = "Add Muscle Group";
-            label1.Text = "Enter Muscle Group to add to the database.";
-            gbAddUpdateDelete.Tag = "AddMG";
-            btnAddUpdate.Text = "Add to Muscle Groups";
-            gbAddUpdateDelete.Visible = true;
+            SetGroupBox("Add Muscle Group", "Enter Muscle Group to add to the database.", "AddMG", "Add to Muscle Groups");
+            MakeGroupBoxVisible();
+        }
 
+        private void tsmUpdateMG_Click(object sender, EventArgs e)
+        {
+            using WorkoutBuilderContext context = new();
+            SetGroupBox("Update Muscle Groups", "Select a Muscle Group to Update", "UpdateMG", "Update");
+            MakeGroupBoxVisible();
+            List<WorkoutPart> workoutParts = context.WorkoutParts.ToList();
+            FillMuscleGroupComboBox(workoutParts);
+            MakeComboBoxVisible();
         }
 
         /// <summary>
         /// Adds the value from the group text box
         /// to the muscle group table in the database.
         /// </summary>
-        /// <param name="text">The group box text box</param>
-        public void addMuscleGroup(TextBox text)
+        /// <param name="muscleGroupToAdd">The group box text box</param>
+        public void AddMuscleGroup(string muscleGroupToAdd)
         {
             WorkoutBuilderContext context = new();
             WorkoutPart muscleGroup = new()
             {
-                MuscleGroup = text.Text
+                MuscleGroup = muscleGroupToAdd
             };
             context.WorkoutParts.Add(muscleGroup);
             context.SaveChanges();
-            
         }
+
+        /// <summary>
+        /// Sets the values of the Group Box elements
+        /// passed to the strings that are passed in
+        /// </summary>
+        /// <param name="grpBoxText">Value of the Group box text</param>
+        /// <param name="lblText">Value the label is to display</param>
+        /// <param name="grpBoxTag">the string the tag is to hold</param>
+        /// <param name="btnText">Value the button is to display</param>
+        private void SetGroupBox(string grpBoxText, string lblText, string grpBoxTag, string btnText)
+        {
+            gbAddUpdateDelete.Text = grpBoxText;
+            label1.Text = lblText;
+            gbAddUpdateDelete.Tag = grpBoxTag;
+            btnAddUpdate.Text = btnText;
+        }
+
+        /// <summary>
+        /// Makes the Group box visible
+        /// </summary>
+        private void MakeGroupBoxVisible()
+        {
+            gbAddUpdateDelete.Visible=true;
+        }
+
+        /// <summary>
+        /// Makes the combo box visible
+        /// </summary>
+        private void MakeComboBoxVisible()
+        {
+            cbUpdateDelete.Visible = true;
+        }
+
+        private void FillMuscleGroupComboBox(List<WorkoutPart> list)
+        {
+            foreach (WorkoutPart musclegroup in list)
+            {
+                cbUpdateDelete.Items.Add(musclegroup.MuscleGroup);
+            }
+        }
+
 
         // TODO:
         // Exercise: add, update and delete functionality
